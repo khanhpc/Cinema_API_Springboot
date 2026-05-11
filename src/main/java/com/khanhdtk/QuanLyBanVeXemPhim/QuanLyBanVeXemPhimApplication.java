c6ad500d9cd4b2dfacab0800b487cda6dacb1bad
khanhpc <khanh2k5pc@gmail.com>
2026-05-11 13:23:15 +0700

    11-5-2026

--- src/main/java/com/khanhdtk/QuanLyBanVeXemPhim/QuanLyBanVeXemPhimApplication.java
+++ src/main/java/com/khanhdtk/QuanLyBanVeXemPhim/QuanLyBanVeXemPhimApplication.java
@@ -1,13 +1,20 @@
 package com.khanhdtk.QuanLyBanVeXemPhim;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class QuanLyBanVeXemPhimApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(QuanLyBanVeXemPhimApplication.class, args);
 	}
